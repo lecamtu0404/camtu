05e085ae1f5b97b61eb376ae9b17667a4927eb6b
lecamtu0404 <lecamtu30042004@gmail.com>
2023-03-02 18:03:47 +0700

    Main

---
+++ Chuong3/BTCN/Bai3.4.py
@@ -0,0 +1,12 @@
+a=float(input())
+b=float(input())
+c=float(input())
+d=(a*2+b*3+c)/6
+if d<3:
+    print("Kem")
+elif d<5: print("Yeu")
+elif d<6: print("Trung binh")
+elif d<7: print("Trung binh Kha")
+elif d<8: print("Kha")
+elif d<9: print("Gioi")
+else: print("Xuat sac")
