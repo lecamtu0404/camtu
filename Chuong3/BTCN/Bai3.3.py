ac9df15d81c558bc047e1b3b1dc263c0a8d6979b
lecamtu0404 <lecamtu30042004@gmail.com>
2023-03-07 11:11:49 +0700

    Main

--- Chuong3/BTCN/Bai3.3.py
+++ Chuong3/BTCN/Bai3.3.py
@@ -1,10 +1,9 @@
 x=float(input("x="))
 y=float(input("y="))
 ch=input("Phep toan:")
-if ch=="+":
-    c=x+y
-elif ch=="-": c=x-y
-elif ch=="*": c=x*y
-elif ch=="/" and y!=0 : c=x/y
+if ch=="+": print(str(x)+str(ch)+str(y)+"="+str(round(x+y,1)))
+elif ch=="-": print(str(x)+str(ch)+str(y)+"="+str(round(x-y,1)))
+elif ch=="*": print(str(x)+str(ch)+str(y)+"="+str(round(x*y,1)))
+elif ch=="/" and y!=0: print(str(x)+str(ch)+str(y)+"="+str(round(x/y,1)))
+elif ch=="/" and y==0: print("Khong hop le")
 else: print("Khong hop le")
-print(str(x)+str(ch)+str(y)+"="+str(round(c,1)))
---
+++ Chuong3/P2.Cau3.1.py
@@ -0,0 +1,4 @@
+n=int(input("Nhap so nguyen n: "))
+while n<1 or n>=51:
+    print("Moi nhap lai")
+    n=int(input("n="))
